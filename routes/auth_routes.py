0d0b9289ccd140eabedc07ee5ad691556693a87a
gabitenoriob <gabitenoriob@gmail.com>
2025-05-26 11:28:46 -0300

    feat: sign up user endpoint

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,7 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from database import SessionLocal,engine, get_db, Base
+from grpc import Status
+from models import User
+from routes.schemas import SignUpModel
+from sqlalchemy.orm import sessionmaker, declarative_base
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth_router = APIRouter(prefix= '/auth', tags= ['auth'])
-
+session = SessionLocal(bind=engine)
 @auth_router.get('/')
 async def hello():
     return "hello"
+
+@auth_router.post('/sign-up', response_model=SignUpModel, status_code=Status.HTTP_201_CREATED)
+async def sign_up(user: SignUpModel):
+    db_email = session.query(User).filter(User.email == user.email).first()
+    if db_email:
+        raise HTTPException(status_code=Status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    
+    db_username = session.query(User).filter(User.username == user.username).first()
+    if db_username:
+        raise HTTPException(status_code=Status.HTTP_400_BAD_REQUEST, detail="Username already taken")
+    
+    user.password = generate_password_hash(user.password, method='sha256')
+    new_user = User(**user.model_dump())
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+    return new_user
---
+++ routes/schemas.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+from sqlalchemy import false, true
+
+class SignUpModel(BaseModel):
+    id: Optional[int]
+    username : str
+    email : str
+    password : str
+    is_active : Optional[bool] 
+    is_staff : Optional[bool] 
+
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "username": "john_doe",
+                "email": "john_doe@example.com",
+                "password": "securepassword",
+                "is_active": True,
+                "is_staff": False
+            }
+        }
